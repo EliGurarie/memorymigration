434768122373ba01f3fb742e59582e33ceaf3c13
spot3512 <spot3512@terpmail.umd.edu>
2021-01-16 14:54:24 -0500

    Changed X.mid to X

--- code/MinimalWorkingExample.R
+++ code/MinimalWorkingExample.R
@@ -2,12 +2,12 @@ source("functions2.R")
 
 world <- getSinePop(tau = 100, X.min = 0, X.max = 100, dx=1, 
                     peak.max = 80, peak.min = 20, sd = 10)
-world$resource <- getPulsedResource(world$time, world$X.mid, c(t.peak = 25, t.sd = 9, x.peak = 80, x.sd = 9))
+world$resource <- getPulsedResource(world$time, world$X, c(t.peak = 25, t.sd = 9, x.peak = 80, x.sd = 9))
 
 
 parameters <- c(epsilon = 5, alpha = 300, beta0=100, beta1 = 300)
 world.R1 <- world %>% 
-  list_modify(resource = getPulsedResource(1:world$tau, world$X.mid, c(t.peak = 25, t.sd = 9, x.peak = 80, x.sd = 9)))
+  list_modify(resource = getPulsedResource(1:world$tau, world$X, c(t.peak = 25, t.sd = 9, x.peak = 80, x.sd = 9)))
 M <- runManyYears(world.R1, Parameters = parameters, n.years = 6)
 
 plotYearList(M, tau = tau)
@@ -45,14 +45,14 @@ sqrt(mean((M2[[6]]-M2[[5]])^2))
 # Social cohesion index
   
   getSD <- function(f, world){
-    EX <- sum( world$X.mid * f) * world$dx
-    EX2 <- sum( world$X.mid^2 * f) * world$dx
+    EX <- sum( world$X * f) * world$dx
+    EX2 <- sum( world$X^2 * f) * world$dx
     sd  = sqrt(EX2 - EX^2)
   }
 
   computeCohesiveness <- function(P){
     SDs <- apply(P, 1, getSD, world = world)
-    SD.max <- (1/sqrt(12))*max(world$X.mid)
+    SD.max <- (1/sqrt(12))*max(world$X)
     SC <- 1 - SDs/SD.max
     c(SC.mean = mean(SC), SC.sd = sd(SC))
   }
