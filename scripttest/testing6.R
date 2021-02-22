3b311a2150c8c961e09ef1d5e740b05b45e83578
spot3512 <spot3512@terpmail.umd.edu>
2021-02-22 18:04:11 -0500

    deleted test files

--- scripttest/testing6.R
+++
@@ -1,8 +0,0 @@
-require(memorymigration)
- data(world); data(resources)
- parameters = c( epsilon=5, alpha=5, beta0=2, beta1=2 )
- world$resource <-  resource_R1 
- M <- runManyYears(world, Parameters = parameters, n.years = 30, threshold = 0.99) 
- indices <- computeIndices(M[[length(M)]], world$resource, world) 
- R6 <- data.frame(t(parameters), indices) 
- save( R6, file =paste0('scripttest/results/run6.rda'))
--- testing.R
+++
@@ -1,9 +0,0 @@
-require(memorymigration)
- load(world)
- load(resource)
- parameters.df = c( epsilon=10, alpha=300, beta0=100, beta1=300 )
- world$resource <- resourcename 
- M <- runManyYears(world, Parameters = parameters, n.years = 30, threshold = 0.99) 
- indices <- computeIndices(M[[length(M)]], world$resource, world) 
- R2 <- data.frame(t(parameters), indices) 
- save(R2, file = paste0( . results/run2.rda
