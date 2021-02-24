ff83f453b5528f613046c25c71b100380bb2f4d2
Elie Gurarie <egurarie@umd.edu>
2021-02-24 13:20:00 -0600

    sdfgdf

--- memorymigration/R/generateScripts.R
+++ memorymigration/R/generateScripts.R
@@ -40,7 +40,7 @@
 
 
 createSource <- function(worldname = "world", resourcename, 
-                         directory, filename, 
+                         code.dir, filename, results.dir,
                          epsilon, depsilon, alpha, dalpha, 
                          beta0, dbeta0, beta1, dbeta1){
   runparametersplit <- parameterGrid(epsilon, depsilon, alpha, dalpha, 
@@ -55,7 +55,7 @@ createSource <- function(worldname = "world", resourcename,
              beta0,",", dbeta0,",", beta1,",", dbeta1,")\n"))
     cat(paste0("parameters.df",i, "= parametersplit[[",i,"]]\n"),
         "results <- runManyRuns(",paste0("parameters.df",i,", world)\n"),
-        "save(results, file =",paste0("(paste0('scripttest/results/run",i,".rda')))\n"))
+        "save(results, file =",paste0("paste0('",results.dir,"/run",i,".rda'))\n"))
     sink()}
 }
 
