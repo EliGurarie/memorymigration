5ae2707eafbd819808a86d1a4bee5cd0f516f28c
Elie Gurarie <egurarie@umd.edu>
2021-06-21 15:00:21 -0500

    exploring ...

--- code/analysis/01_StabilityAnalysis.R
+++ code/analysis/01_StabilityAnalysis.R
@@ -1,5 +1,9 @@
 require(memorymigration)
 require(plyr)
+require(gplots)
+require(ggplot2)
+require(ggthemes)
+require(gridExtra)
 
 fixTE <- function(df){
   df %>% mutate(TE = t1.error + t2.error + abs(x1.error) + abs(x2.error), 
@@ -7,74 +11,64 @@ fixTE <- function(df){
          Migration = ifelse(is.na(Migration), "bad", Migration))
 }
 
-a <- load("results/stability/stability_1.rda")
+load("results/stability/stability_1.rda")
 eps1 <- newresults %>% fixTE
+load("results/stability/stability_1b.rda")
+eps1 <- rbind(eps1, newresults %>% fixTE)
+
 load("results/stability/stability_4.rda")
 eps4 <- newresults %>% fixTE
+load("results/stability/stability_4b.rda")
+eps4 <- rbind(eps4, newresults %>% fixTE)
+
 load("results/stability/stability_8.rda")
 eps8 <- newresults %>% fixTE
+load("results/stability/stability_8b.rda")
+eps8 <- rbind(eps8, newresults %>% fixTE)
 
 
 
-ggResults <- function(df, ...){
-  
-  require(gplots)
-  require(ggplot2)
-  require(ggthemes)
-  require(gridExtra)
-  
-  #cuts <- c(0, 2^(seq(-1,10,1)))
-  
+ggMigration <- function(df,  cols = c("darkblue", "orange", "darkgrey"), ...){
   df %>% 
     ggplot(aes(sigma_t, sigma_x, fill = factor(Migration))) + 
     facet_grid(beta~alpha) + 
-    #scale_fill_gradientn(trans = "log", breaks = cuts, colours = rich.colors(100)[100:1]) + 
-    scale_fill_manual(values = c("darkblue", "grey", "white")) + 
+    scale_fill_manual(values = cols) + 
     geom_tile() + theme_few() + 
     xlab("") + theme(legend.position = "none")
-
-  #  p2 <- df %>% subset(ss) %>% 
-  #   ggplot(aes(sigma_t, sigma_x, fill = n.runs)) + 
-  #    facet_grid(beta~alpha) + 
-  #    geom_tile() + theme_few() + ggtitle("N. runs")  + xlab("")
-    
-  #  p3 <- df %>% subset(ss) %>% 
-  #    ggplot(aes(sigma_t, sigma_x, fill = FE)) + 
-  #    facet_grid(beta~alpha) + scale_fill_gradientn(colours = rich.colors(100)) + 
-  #    geom_tile() + theme_few() + ggtitle("Foraging Efficiency")
-    
-  #  grid.arrange(p1, p3, nrow = 1, ...)
 }
 
 
+ggFE <- function(df, ...){
+  df %>% 
+    ggplot(aes(sigma_t, sigma_x, fill = FE)) + 
+    facet_grid(beta~alpha) + 
+    scale_fill_gradientn(colours = rich.colors(101), limits=c(0,1)) + 
+    geom_tile() + theme_few() + 
+    xlab("") + theme(legend.position = "none")
+}
+
+
+ggFE(subset(eps1, lambda == 50))
+
+require(gtools)
 df <- smartbind(eps1, eps4, eps8)
 
 
-plist <- dlply(df, c("epsilon","lambda"), 
-               function(df) ggResults(df) + 
+p.migration.list <- dlply(df, c("epsilon","lambda"), 
+               function(df) ggMigration(df) + 
                  ggtitle(paste("epsilon =", df$epsilon[1], ";", paste("lambda =", df$lambda[1]))))
+
+p.FE.list <- dlply(df, c("epsilon","lambda"), 
+                   function(df) ggFE(df) + 
+                     ggtitle(paste("epsilon =", df$epsilon[1], ";", paste("lambda =", df$lambda[1]))))
 
 
 pdf("plots/StabilityResults.pdf", height = 10, width = 12)
-grid.arrange(plist[[1]], plist[[2]], plist[[3]], 
-             plist[[4]], plist[[5]], plist[[6]], 
-             plist[[7]], plist[[8]],
-             ncol = 3)
+grid.arrange(grobs = p.migration.list, ncol = 3)
+grid.arrange(grobs = p.FE.list, ncol = 3)
 dev.off()
 
-pdf("plots/StabilityResults.pdf", height = 11, width = 8)
-ggResults(eps1, top = "epsilon = 1")
-ggResults(eps4, top = "epsilon = 4")
-ggResults(eps8, top = "epsilon = 8")
-dev.off()
 
-table(eps1$lambda)
-
-pdf("plots/StabilityResults_eps1_bylambda.pdf", height = 6, width = 11)
-ggResults(eps1 %>% subset(lambda == 20), top = "epsilon = 1, lambda = 20")
-ggResults(eps1 %>% subset(lambda == 50), top = "epsilon = 1, lambda = 50")
-ggResults(eps1 %>% subset(lambda == 100), top = "epsilon = 1, lambda = 100")
-dev.off()
 
 
 
@@ -96,36 +90,48 @@ hist(eps4$TE[eps4$TE < 7], breaks = 50)
 
 
 
-
-summary(TE.lm)
-
 require(broom)
 require("GGally")
 
-coefs.eps1 <- glm(I(TE < 2) ~ (scale(alpha) + 
-                                          scale(beta) + 
-                                          scale(sigma_t) + 
-                                          scale(sigma_x) + 
-                                          (factor(lambda) - 1))^2, 
-                           data = eps1 %>% subset(lambda < 100), family = "binomial") %>% 
+coefs.df <- glm(I(TE < 2) ~ (scale(alpha) + 
+                              scale(beta) + 
+                              scale(sigma_t) + 
+                              scale(sigma_x) + 
+                              (factor(epsilon) - 1) + 
+                              (factor(lambda) - 1))^2, 
+                           data = df, family = "binomial") %>% 
   tidy(conf.int = TRUE) 
 
 
-
-coefs.eps4 <- glm(I(TE < 2) ~ (scale(alpha) + 
-                                 scale(beta) + 
-                                 scale(sigma_t) + 
-                                 scale(sigma_x) + 
-                                 (factor(lambda) - 1))^2, 
-                  data = eps4, family = "binomial") %>% 
-  tidy(conf.int = TRUE) 
-
-
-coefs.eps1 %>% mutate(term = gsub("scale", "", term ), 
-         term = gsub("factor", "", term )) %>%  
+coefs.df %>% mutate(term = gsub("scale", "", term )) %>% 
+  mutate(term = gsub("factor", "", term, fixed = TRUE)) %>%  
   ggcoef()
 
-coefs.eps4 %>% mutate(term = gsub("scale", "", term ), 
-                      term = gsub("factor", "", term )) %>%  
-  ggcoef()
 
+
+
+# 
+require(randomForest)
+names(df)
+features <- c("epsilon","alpha","beta","lambda","sigma_x","sigma_t","TE")
+
+system.time(
+migration.rf <- randomForest(factor(I(TE < 2)) ~ ., 
+                             data=df[,features] %>% 
+                               mutate(epsilon = factor(epsilon),
+                                      lambda = factor(lambda)), 
+                             ntree=1000,
+                             keep.forest=FALSE, 
+                             importance=TRUE)
+
+)
+
+varImpPlot(migration.rf)
+
+require(rpart)
+migration.dt <- rpart(factor(I(TE < 2)) ~ ., 
+                      data=df[,features] %>% mutate(epsilon = factor(epsilon),
+                                                    lambda = factor(lambda)))
+
+plot(migration.dt)
+text(migration.dt, use.n = TRUE)
--- code/analysis/02_LearningMigrationAnalysis.R
+++ code/analysis/02_LearningMigrationAnalysis.R
@@ -3,31 +3,31 @@ require(plyr)
 
 fixTE <- function(df){
   df %>% mutate(TE = abs(t1.error) + abs(t2.error) + abs(x1.error) + abs(x2.error), 
-                Migration = cut(TE, c(0,1,5,10,1e3)))
+                Migration = cut(TE, c(1e3,32,8,2,0), label = LETTERS[1:4]) %>% 
+                  relevel("B") %>% relevel("C") %>% relevel("D"))
 }
+
+
 
 load("results/migratory/migratory_1.rda")
 eps1 <- newresults %>% fixTE
 load("results/migratory/migratory_4.rda")
 eps4 <- newresults %>% fixTE
+load("results/migratory/migratory_8.rda")
+eps8 <- newresults %>% fixTE
 
+require(gtools)
+df <- smartbind(eps1, eps4, eps8)
 
-
-ggResults <- function(df, ...){
-  
-  require(gplots)
-  require(ggplot2)
-  require(ggthemes)
-  require(gridExtra)
-
+ggMigration <- function(df,  cols = rich.colors(4), ...){
   df %>% 
     ggplot(aes(sigma_t, sigma_x, fill = factor(Migration))) + 
     facet_grid(beta~alpha) + 
-    #scale_fill_gradientn(trans = "log", breaks = cuts, colours = rich.colors(100)[100:1]) + 
-    scale_fill_manual(values = rich.colors(4)) + 
+    scale_fill_manual(values = cols) + 
     geom_tile() + theme_few() + 
-    xlab("") #+ theme(legend.position = "none")
+    xlab("") + theme(legend.position = "none")
 }
+
 
 
 ggN.runs <- function(df, ...){
@@ -41,28 +41,29 @@ ggN.runs <- function(df, ...){
     xlab("") + theme(legend.position = "none")
 }
 
-df <- smartbind(eps1, eps4)
 
-
-plist <- dlply(df, c("epsilon","lambda"), 
-               function(df) ggResults(df) + 
+migration.plist <- dlply(df, c("epsilon","lambda"), 
+               function(df) ggMigration(df) + 
                  ggtitle(paste("epsilon =", df$epsilon[1], ";", paste("lambda =", df$lambda[1]))))
 
 nruns.plist <- dlply(df, c("epsilon","lambda"), 
                      function(df) ggN.runs(df) + 
                        ggtitle(paste("epsilon =", df$epsilon[1], ";", paste("lambda =", df$lambda[1]))))
 
-pdf("plots/LearningMigration_nruns.pdf", height = 10, width = 12)
-grid.arrange(grobs = nruns.plist, ncol = 3)
-dev.off()
-
 
 pdf("plots/LearningMigrationResults.pdf", height = 10, width = 12)
-grid.arrange(grobs = plist, ncol = 3)
+grid.arrange(grobs = migration.plist, ncol = 3, top = "Migration")
+#grid.arrange(grobs = nruns.plist, ncol = 3, top = "N. runs")
 dev.off()
 
-t
 
+
+boxplot(MI ~ Migration * lambda, data =df, col = 2:5)
+
+ggplot(df, aes(sigma_x, TE, col = factor(beta), 
+               pch = factor(alpha))) + 
+  geom_point(alpha = 0.5) + facet_grid(lambda ~ epsilon) + 
+  scale_y_continuous(trans='log2')  + theme_few()
 
 
 summary(TE.lm)
@@ -70,30 +71,32 @@ summary(TE.lm)
 require(broom)
 require("GGally")
 
-coefs.eps1 <- glm(I(TE < 2) ~ (scale(alpha) + 
-                                 scale(beta) + 
-                                 scale(sigma_t) + 
-                                 scale(sigma_x) + 
-                                 (factor(lambda) - 1))^2, 
-                  data = eps1 %>% subset(lambda < 100), family = "binomial") %>% 
-  tidy(conf.int = TRUE) 
 
+# 
+require(randomForest)
+names(df)
 
+system.time(
+  learning.rf <- randomForest(Migration ~ ., 
+                               data=df[,features] %>% 
+                                 mutate(epsilon = factor(epsilon),
+                                        lambda = factor(lambda)), 
+                               ntree=1000,
+                               keep.forest=FALSE, 
+                               importance=TRUE)
+  
+)
 
-coefs.eps4 <- glm(I(TE < 2) ~ (scale(alpha) + 
-                                 scale(beta) + 
-                                 scale(sigma_t) + 
-                                 scale(sigma_x) + 
-                                 (factor(lambda) - 1))^2, 
-                  data = eps4, family = "binomial") %>% 
-  tidy(conf.int = TRUE) 
+varImpPlot(learning.rf)
 
+require(rpart)
 
-coefs.eps1 %>% mutate(term = gsub("scale", "", term ), 
-                      term = gsub("factor", "", term )) %>%  
-  ggcoef()
+features <- c("epsilon","alpha","beta","lambda","sigma_x","sigma_t", "Migration")
 
-coefs.eps4 %>% mutate(term = gsub("scale", "", term ), 
-                      term = gsub("factor", "", term )) %>%  
-  ggcoef()
+learning.dt <- rpart(Migration ~ ., 
+                      data=df[,features] %>% mutate(epsilon = factor(epsilon),
+                                                    lambda = factor(lambda)))
+
+plot(learning.dt)
+text(learning.dt, use.n = TRUE)
 
