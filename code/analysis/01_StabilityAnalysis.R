ab7547557552b1a22ff1c4c36adbc6ffe61afa3b
Eliezer Gurarie <egurarie@russell.wisc.edu>
2021-06-22 16:12:43 -0500

    new resultys / figure

--- code/analysis/01_StabilityAnalysis.R
+++ code/analysis/01_StabilityAnalysis.R
@@ -4,28 +4,38 @@ require(gplots)
 require(ggplot2)
 require(ggthemes)
 require(gridExtra)
-
-fixTE <- function(df){
-  df %>% mutate(TE = t1.error + t2.error + abs(x1.error) + abs(x2.error), 
-         Migration = cut(TE, c(0,1,7,1e3), labels = c("perfect", "good", "bad")), 
-         Migration = ifelse(is.na(Migration), "bad", Migration))
+eval <- FALSE
+if(eval){
+  fixTE <- function(df){
+    df %>% mutate(TE = t1.error + t2.error + abs(x1.error) + abs(x2.error), 
+           Migration = cut(TE, c(0,1,7,1e3), labels = c("perfect", "good", "bad")), 
+           Migration = ifelse(is.na(Migration), "bad", Migration))
+  }
+  
+  load("results/stability/stability_1.rda")
+  eps1 <- newresults %>% fixTE
+  load("results/stability/stability_1b.rda")
+  eps1 <- rbind(eps1, newresults %>% fixTE)
+  load("results/stability/stability_1c.rda")
+  eps1 <- rbind(eps1, newresults %>% fixTE)
+  
+  load("results/stability/stability_4.rda")
+  eps4 <- newresults %>% fixTE
+  load("results/stability/stability_4b.rda")
+  eps4 <- rbind(eps4, newresults %>% fixTE)
+  load("results/stability/stability_4c.rda")
+  eps4 <- rbind(eps4, newresults %>% fixTE)
+  
+  load("results/stability/stability_8.rda")
+  eps8 <- newresults %>% fixTE
+  load("results/stability/stability_8b.rda")
+  eps8 <- rbind(eps8, newresults %>% fixTE)
+  load("results/stability/stability_8c.rda")
+  eps8 <- smartbind(eps8, newresults %>% fixTE)
+  
+  stability <- smartbind(eps1, eps4, eps8)
+  save(df, file = "results/stability/stability_compiled.rda")
 }
-
-load("results/stability/stability_1.rda")
-eps1 <- newresults %>% fixTE
-load("results/stability/stability_1b.rda")
-eps1 <- rbind(eps1, newresults %>% fixTE)
-
-load("results/stability/stability_4.rda")
-eps4 <- newresults %>% fixTE
-load("results/stability/stability_4b.rda")
-eps4 <- rbind(eps4, newresults %>% fixTE)
-
-load("results/stability/stability_8.rda")
-eps8 <- newresults %>% fixTE
-load("results/stability/stability_8b.rda")
-eps8 <- rbind(eps8, newresults %>% fixTE)
-
 
 
 ggMigration <- function(df,  cols = c("darkblue", "orange", "darkgrey"), ...){
@@ -47,11 +57,119 @@ ggFE <- function(df, ...){
     xlab("") + theme(legend.position = "none")
 }
 
+plotBlock <- function(block, cols = c("darkblue", "orange", "lightgrey"), 
+                      side1 = FALSE, side2 = FALSE){
+  require(reshape2)
+  b <- block[,c("TE", "alpha", "beta", "sigma_t", "sigma_x")]
+  ns <- apply(b[,-1], 2, function(x) length(unique(x)))
+  
+  M <-   b %>% acast(alpha + sigma_t ~ beta +  sigma_x, 
+                     value.var = "TE")
+  
+  #M[is.na(M)] <- max(M, na.rm = TRUE)
+  image(1:ncol(M)-.5, 1:nrow(M)-.5, M, breaks = c(0,1,5,1000), 
+        col = cols, xaxt ="n", xlab = "", ylab = "", yaxt = "n")
+  abline(v = 1:ncol(M), col = "white")
+  abline(h = 1:nrow(M), col = "white")
+  abline(v = (0:ns["alpha"]) * ns["sigma_t"], lwd = 2)
+  abline(h = (0:ns["beta"]) * ns["sigma_x"], lwd = 2)
+  
+  if(side1)
+    text(1:ns["alpha"] * ns["sigma_t"] - ns["sigma_t"]/2, -1, 
+        unique(b$alpha), xpd = NA)
+  
+  if(side2)
+    text(nrow(M)+ 1, 
+        1:ns["beta"] * ns["sigma_x"] - ns["sigma_x"]/2,
+        unique(b$beta), xpd = NA, srt = 270)
+}
+
+
+
+PlotMiniblock <- function(){
+  miniM <- subset(df, 
+                  lambda == 100 & epsilon == 1 & 
+                    beta == 400 & alpha == 200)[,c("sigma_t", "sigma_x", "TE")] %>% 
+    acast(sigma_t ~ sigma_x)
+  
+  sigs <- seq(3,15,3)
+  image(sigs - 1.5, sigs - 1.5, miniM, breaks = c(0,1,5,1000), 
+        col = cols, xaxt ="n", xlab = "", ylab = "", yaxt = "n", asp = 1)
+  mtext(side = 1, at = sigs-1.5, sigs, line = .5, cex = 1.15)
+  mtext(side = 2, at = sigs-1.5, sigs, line = .5, cex = 1.15)
+  abline(v = sigs, col = "white")
+  abline(h = sigs, col = "white")
+  mtext(side = 1, expression("Resource duration "~(sigma[t])), line = 3, cex = 1.25)
+  mtext(side = 2, expression("Resource extent "~(sigma[x])), line = 2, cex = 1.25)
+}
+
+
+plotStability <- function(stability){
+  layout(rbind(1:3,4:6) %>% cbind(7:8))
+  par( mar = c(1,1,1,1)/2, oma = c(3,3,3,3), xpd = FALSE)
+  for(e in c(1,8)) for(l in c(20,50,100)){
+    myblock <- subset(stability, epsilon == e & lambda == l) 
+    if(e == 1){
+      if(l == 20){
+        plotBlock(myblock)
+        mtext(side = 3, expression("tight swarm "~(lambda == 20)), line = 1, cex = 1.25)
+        mtext(side = 2, expression("low diffusion "~(epsilon == 1)), line = 1, cex = 1.25)
+      }
+      if(l == 50){
+        plotBlock(myblock)
+        mtext(side = 3, expression("medium swarm"~(lambda == 40)), line = 1, cex = 1.25)
+      }
+      if(l == 100){
+        plotBlock(myblock, side2 = TRUE)
+        mtext(side = 3, expression("loose swarm"~(lambda == 100)), line = 1, cex = 1.25)
+      }} 
+    else{ 
+      if(l == 20){  
+        plotBlock(myblock, side1 = TRUE)
+        mtext(side = 2, expression("high diffusion "~(epsilon == 8)), line = 1, cex = 1.25)
+      }
+      if(l == 50) plotBlock(myblock, side1 = TRUE)
+      if(l == 100){  
+        plotBlock(myblock, side1 = TRUE, side2 = TRUE)
+      }
+    }
+  }
+  b <- myblock[,c("TE", "alpha", "beta", "sigma_t", "sigma_x")]
+  ns <- apply(b[,-1], 2, function(x) length(unique(x)))
+  
+  mtext(side = 1, outer = TRUE, expression("resource following "~(alpha)), line = 1.5, cex = 1.25)
+  text(ns["sigma_t"] * ns["alpha"]  + 3, 
+       ns["sigma_x"] * ns["beta"], srt = 270, xpd = NA, 
+       cex = 2, 
+       expression("social attraction " ~(beta)))
+
+  par(mar = c(4,8,4,0))  
+  plot.new()
+  legend("center", fill = cols, legend = c("< 1", "1-5", ">1"), cex = 2, title = "total error", bty = "n")
+  PlotMiniblock()
+}
+
+load("results/stability/stability_compiled.rda")
+png("plots/StabilityResults.png", width = 3800, height = 2000, res = 300)
+plotStability(stability)
+dev.off()
+
+
+
+
+
+
+
+ns <- apply(b[,-1], 2, function(x) length(unique(x)))
+
+M <-   b %>% acast(alpha + sigma_t ~ beta +  sigma_x, 
+                   value.var = "TE")
+
+
 
 ggFE(subset(eps1, lambda == 50))
 
 require(gtools)
-df <- smartbind(eps1, eps4, eps8)
 
 
 p.migration.list <- dlply(df, c("epsilon","lambda"), 
